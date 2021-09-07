a23fad0c27e263021e3586f74a827389abea1071
Matthew Parisien <matthewparisien4@gmail.com>
2021-09-07 18:59:04 +0000

    Wrap function in jquery document.ready

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,21 @@
-const returnValidCharacters = function () {};
+$(document).ready(() => {
 
-const incrementCharCount = function (e) {
-  let characterCount = document.querySelector(".counter");
-  let counter = Number(characterCount.innerHTML);
-  
-  if (e.key === 'Backspace' && counter === 0) {
-    characterCount.innerHTML = 0;
-    return characterCount;
-  } else if (e.key === 'Backspace') {
-    counter -= 1;
+  const incrementCharCount = function (e) {
+    let characterCount = document.querySelector(".counter");
+    let counter = Number(characterCount.innerHTML);
+
+    if (e.key === "Backspace" && counter === 0) {
+      characterCount.innerHTML = 0;
+      return characterCount;
+    } else if (e.key === "Backspace") {
+      counter -= 1;
+      characterCount.innerHTML = counter.toString();
+      return characterCount;
+    }
+
+    counter++;
     characterCount.innerHTML = counter.toString();
     return characterCount;
-  }
-
-  counter++;
-  characterCount.innerHTML = counter.toString();
-  return characterCount;
-};
+  };
+  
+});
