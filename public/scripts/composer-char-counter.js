68bbddb0722ee2bce2ed7f7bbc9fe555cee2b3a7
Matthew Parisien <matthewparisien4@gmail.com>
2021-09-08 14:24:29 +0000

    Add class to output element when number hits below 0 and style the color to be red (signals that the character limit has exceeded.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,12 +1,16 @@
 $(document).ready(() => {
-  $('#tweet-text').on('keyup', function(e) {
-
+  $("#tweet-text").on("keyup", function () {
     let maxCharNum = 140;
-    let counter = $($(this).next()[0]).children('#counter')[0];
+    let counter = $($(this).next()[0]).children("#counter")[0];
     const charCount = this.value.length;
-
     maxCharNum = maxCharNum - charCount;
-    $(counter).html(maxCharNum.toString());
 
-  })
-})
+    if (maxCharNum < 0) {
+      $(counter).addClass('charLimitExceeded');
+    } else {
+      $(counter).removeClass('charLimitExceeded');
+    }
+
+    $(counter).html(maxCharNum.toString());
+  });
+});
