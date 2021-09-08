9f79d2e48f94eac7922cac3c92d5e85bbda392c9
Matthew Parisien <matthewparisien4@gmail.com>
2021-09-08 14:16:32 +0000

    Use key up instead of keypress to prevent bugs

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,23 +1,12 @@
 $(document).ready(() => {
-  $("#tweet-text").on("keydown", function (e) {
-    const counter = $($(this).next()[0]).children("#counter")[0];
-    let number = Number(counter.innerHTML);
+  $('#tweet-text').on('keyup', function(e) {
 
-    if (number <= 0) {
-      $(counter).addClass('charLimitExceeded');
-    } else {
-      $(counter).removeClass('charLimitExceeded');
-    }
-    
-    if (e.key === 'Backspace') {
-      number += 1
-      counter.innerHTML = number;
-      return;
-    } 
+    let maxCharNum = 140;
+    let counter = $($(this).next()[0]).children('#counter')[0];
+    const charCount = this.value.length;
 
-  
-    number -= 1;
-    counter.innerHTML = number;
-    console.log(number);
-  });
-});
+    maxCharNum = maxCharNum - charCount;
+    $(counter).html(maxCharNum.toString());
+
+  })
+})
