6bd46af2307b67aa71d50e052507842e9f42b7f8
Matthew Parisien <matthewparisien4@gmail.com>
2021-09-10 20:48:50 +0000

    Clean spacing/syntax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,32 +1,31 @@
 $(document).ready(function () {
-  
   const data = [
     {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
+      user: {
+        name: "Newton",
+        avatars: "https://i.imgur.com/73hZDYK.png",
+        handle: "@SirIsaac",
       },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
+      content: {
+        text: "If I have seen further it is by standing on the shoulders of giants",
       },
-      "created_at": 1461116232227
+      created_at: 1461116232227,
     },
     {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
+      user: {
+        name: "Descartes",
+        avatars: "https://i.imgur.com/nlhLi3I.png",
+        handle: "@rd",
       },
-      "created_at": 1461113959088
-    }
-  ]
+      content: {
+        text: "Je pense , donc je suis",
+      },
+      created_at: 1461113959088,
+    },
+  ];
 
   /**
-   * 
+   *
    * @param {*} data An object containing user and content data about a new tweet post
    * @returns HTML markup for displaying a new tweet
    */
@@ -36,7 +35,9 @@ $(document).ready(function () {
     <article class="tweet-item">
           <header class="user-info">
             <div class="header-left">
-              <img src="${data.user.avatars}" style="width: 60px; height: 60px;">
+              <img src="${
+                data.user.avatars
+              }" style="width: 60px; height: 60px;">
               <h4>${data.user.name}</h4>
             </div>
             <h4 class="handle">${data.user.handle}</h4>
@@ -67,14 +68,12 @@ $(document).ready(function () {
     return $tweetTemplate;
   };
 
-  const $tweet = createTweetElement(tweetData);
-  $('.display-tweets-container').append($tweet);
-
-  const renderTweets = function(tweets) {
+  const renderTweets = function (tweets) {
     tweets.forEach((tweet) => {
       const newTweet = createTweetElement(tweet);
-      $('.display-tweets-container').append(newTweet);
-    })
-  }
+      $(".display-tweets-container").append(newTweet);
+    });
+  };
 
+  renderTweets(data);
 });
