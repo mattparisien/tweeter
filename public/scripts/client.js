ad109c3d20951b31f3dcfe72ce009eb631ab0beb
Matthew Parisien <matthewparisien4@gmail.com>
2021-09-10 22:00:38 +0000

    Implement new function called loadTweets which makes an ajax get request to the server and returns tweet data. Inside of the request success callback function is the renderTweets function, which is called and passed in data as an argument

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,28 +1,4 @@
 $(document).ready(function () {
-  const data = [
-    {
-      user: {
-        name: "Newton",
-        avatars: "https://i.imgur.com/73hZDYK.png",
-        handle: "@SirIsaac",
-      },
-      content: {
-        text: "If I have seen further it is by standing on the shoulders of giants",
-      },
-      created_at: 1461116232227,
-    },
-    {
-      user: {
-        name: "Descartes",
-        avatars: "https://i.imgur.com/nlhLi3I.png",
-        handle: "@rd",
-      },
-      content: {
-        text: "Je pense , donc je suis",
-      },
-      created_at: 1461113959088,
-    },
-  ];
 
   /**
    *
@@ -79,15 +55,24 @@ $(document).ready(function () {
     });
   };
 
+  //Submit form data via ajax post request
   $("#compose-form").submit(function (e) {
 
     e.preventDefault();
     const $formData = $(this).serialize();
 
     $.post("/tweets/", $formData, function (data, status) {
-      console.log(status);
+      console.log(data);
     });
   });
 
-  renderTweets(data);
+  //Load tweets from server
+  const loadTweets = function() {
+    $.get('/tweets/', function(data) {
+      renderTweets(data);
+    })
+  }
+
+  loadTweets();
+  
 });
