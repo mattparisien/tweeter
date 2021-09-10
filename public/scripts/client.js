7e20f96715a2c88a3d5cb746969cd8b09f7c834b
Matthew Parisien <matthewparisien4@gmail.com>
2021-09-10 20:47:18 +0000

    Implement new function renderTweets which loops through an array of tweet objects, creates a new tweet element for each tweet and appends the output to the display tweet container.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,15 +1,29 @@
 $(document).ready(function () {
-  const tweetData = {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
+  
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  };
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
 
   /**
    * 
@@ -55,5 +69,12 @@ $(document).ready(function () {
 
   const $tweet = createTweetElement(tweetData);
   $('.display-tweets-container').append($tweet);
+
+  const renderTweets = function(tweets) {
+    tweets.forEach((tweet) => {
+      const newTweet = createTweetElement(tweet);
+      $('.display-tweets-container').append(newTweet);
+    })
+  }
 
 });
