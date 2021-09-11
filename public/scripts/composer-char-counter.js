aa8583c60624e4e059e2582630d515ca7cda81b5
Matthew Parisien <matthewparisien4@gmail.com>
2021-09-11 14:36:46 +0000

    Fix bug

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,8 +1,9 @@
 $(document).ready(() => {
   $("#tweet-text").on("input", function (e) {
-
     let dynamicCount = 140;
-    let counter = $($(this).next()[0]).children("#counter")[0];
+    let counter = $(this).parent().find("#counter")[0];
+    console.log(counter)
+    
     const charsTyped = this.value.length;
 
     dynamicCount = dynamicCount - charsTyped;
