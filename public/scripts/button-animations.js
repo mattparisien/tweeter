0de8e1d83bf444b0c76a05c9968bada026f828e8
Matthew Parisien <matthewparisien4@gmail.com>
2021-09-11 18:11:11 +0000

    Add logic to handle back to top button appearance on scroll.

--- public/scripts/button-animations.js
+++ public/scripts/button-animations.js
@@ -1,5 +1,21 @@
-//Adds and removes classes to submit button to animate its circle
+//Button Animations File
 
+//Nav arrow link: On click event triggers a smooth scroll to tweet form
+$(document).ready(function () {
+  const $link = $("#scroll-to-form");
+
+  $($link).on("click", function (e) {
+    e.preventDefault();
+    $("html, body").animate(
+      {
+        scrollTop: $("#compose-form").offset().top - 300, //Offset top by 300px to prevent nav covering form
+      },
+      1500
+    );
+  });
+});
+
+//Adds and removes classes to form submit button to animate its circle
 $(document).ready(() => {
   const button = $("#submit");
   const circle = $("#submit .circle");
@@ -12,5 +28,19 @@ $(document).ready(() => {
   button.mouseleave(function () {
     circle.addClass("circleOut");
     circle.removeClass("circleIn");
+  });
+});
+
+//Reveal back to top button on scroll animation
+$(document).ready(function () {
+  const $backToTop = $(`<a id="backtotop" href="#">Back to Top</a>`).hide();
+  $("body").append($backToTop);
+
+  $(window).on("scroll", function () {
+    if ($(this).scrollTop() >= 1200) {
+      $($backToTop).fadeIn(200);
+    } else if ($(this).scrollTop() <= 1200) {
+      $($backToTop).fadeOut(200);
+    }
   });
 });
