5fbc53909ad8e5280e5b46e2b0c7ffd5db372444
Matthew Parisien <matthewparisien4@gmail.com>
2021-09-13 23:54:09 +0000

    Remove multiple document ready functions and wrap all functions in one.

--- public/scripts/button-animations.js
+++ public/scripts/button-animations.js
@@ -1,76 +1,95 @@
 //Button Animations File
 
+$(() => {
+  
 // Helper function: scrolls to tweet form while taking nav offset into consideration
 /**
- * 
- * @param {*} button Button linking to form 
+ *
+ * @param {*} button Button linking to form
  */
-const scrollToForm = function(e) {
-    e.preventDefault();
-    $("#new-tweet").slideDown();
+ const scrollToForm = function (e) {
+  e.preventDefault();
+  $("#new-tweet").slideDown();
 
-    setTimeout(() => {
-      $("html, body").animate(
-        {
-          scrollTop: $("#compose-form").offset().top - 300, //Offset top by 300px to prevent nav covering form
-        }, 1500);
-        $("#compose-form textarea").focus();
-    }, 400);
-}
+  setTimeout(() => {
+    $("html, body").animate(
+      {
+        scrollTop: $("#compose-form").offset().top - 300, //Offset top by 300px to prevent nav covering form
+      },
+      1500
+    );
+    $("#compose-form textarea").focus();
+  }, 400);
+};
 
 //Nav arrow link: On click event triggers a smooth scroll to tweet form
-$(document).ready(function () {
-  const $button = $("#scroll-to-form");
-  
-  $button.on("click", function(e) {
-    scrollToForm(e);
-  })
-
+const $button = $("#scroll-to-form");
+$button.on("click", function (e) {
+  scrollToForm(e);
 });
 
 //Adds and removes classes to buttons to animate circle background on hover state
-$(document).ready(() => {
-  const button = $("#submit");
-  const circle = $("#submit .circle");
 
-  button.mouseover(function () {
-    circle.removeClass("circleOut");
-    circle.addClass("circleIn");
-  });
+const button = $("#submit");
+const circle = $("#submit .circle");
+const footerButton = ("#signUp");
+const circle2 = ("#signUp .circle");
 
-  button.mouseleave(function () {
-    circle.addClass("circleOut");
-    circle.removeClass("circleIn");
-  });
+button.mouseover(function () {
+  circle.removeClass("circleOut");
+  circle.addClass("circleIn");
 });
+
+button.mouseleave(function () {
+  circle.addClass("circleOut");
+  circle.removeClass("circleIn");
+});
+
+footerButton.mouseover(function () {
+  circle2.removeClass("circleOut");
+  circle2.addClass("circleIn");
+});
+
+footerButton.mouseleave(function () {
+  circle2.addClass("circleOut");
+  circle2.removeClass("circleIn");
+});
+
 
 //Reveal back to top button on scroll animation + hide nav link
-$(document).ready(function () {
-  const $backToTop = $(`<a class="backtotop" href="#">Back to Tweeting!</a>`).hide();
-  const $navLink = $(".nav-right");
-  
-  $("body").append($backToTop);
-  
-  $backToTop.on("click", function(e) {
+const $backToTop = $(
+  `<a class="backtotop" href="#">Back to Tweeting!</a>`
+).hide();
+const $navLink = $(".nav-right");
 
-    scrollToForm(e)
+$("body").append($backToTop);
 
-
-  })
-
-  $(window).on("scroll", function () {
-    
-    if ($(this).scrollTop() >= 1200) {
-      $($backToTop).fadeIn(200);
-    } else {
-      $($backToTop).fadeOut(200);
-    }
-
-    if ($backToTop.is(":visible")) {
-      $($navLink).fadeOut(200);
-    } else {
-      $($navLink).fadeIn(200);
-    }
-
-  });
+$backToTop.on("click", function (e) {
+  scrollToForm(e);
 });
+
+$(window).on("scroll", function () {
+  if ($(this).scrollTop() >= 1200) {
+    $($backToTop).fadeIn(200);
+  } else {
+    $($backToTop).fadeOut(200);
+  }
+
+  if ($backToTop.is(":visible")) {
+    $($navLink).fadeOut(200);
+  } else {
+    $($navLink).fadeIn(200);
+  }
+});
+
+
+})
+
+
+
+
+
+
+
+
+
