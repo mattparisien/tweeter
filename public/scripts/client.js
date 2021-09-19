7a74e037edc4e2ac88b685c61734f6360546949b
Matthew Parisien <matthewparisien4@gmail.com>
2021-09-19 20:57:22 +0000

    Fix linting errors

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,5 +1,4 @@
 $(() => {
-
   $("#root, footer").fadeIn(200);
   /**
    *
@@ -7,12 +6,14 @@ $(() => {
    * @returns HTML markup for displaying a new tweet
    */
 
-  const createTweetElement = function (data) {
+  const createTweetElement = function(data) {
     const $tweetTemplate = `
     <article class="tweet-item">
           <header class="user-info">
             <div class="header-left">
-              <img src="${data.user.avatars}" style="width: 60px; height: 60px;">
+              <img src="${
+                data.user.avatars
+              }" style="width: 60px; height: 60px;">
               <h4>${data.user.name}</h4>
             </div>
             <h4 class="handle">${data.user.handle}</h4>
@@ -49,7 +50,7 @@ $(() => {
    *
    * @param {*} tweets An array of objects containing tweet data.
    */
-  const renderTweets = function (tweets) {
+  const renderTweets = function(tweets) {
     tweets.forEach((tweet) => {
       const newTweet = createTweetElement(tweet);
       $(".display-tweets-container").prepend(newTweet);
@@ -57,48 +58,41 @@ $(() => {
   };
 
   //Submit form data via ajax post request --> get tweets once info has been successfully posted
-  $("#compose-form").submit(function (e) {
+  $("#compose-form").submit(function(e) {
     e.preventDefault();
-    
+
     const $textArea = $("#tweet-text");
     const $formError = $("#compose-form .form-error")[0];
     let $validatedForm = validateForm($textArea);
 
     if ($validatedForm.hasError) {
-      
       e.preventDefault();
       $($formError).text(`${$validatedForm.errorMsg}`).slideDown();
       $($textArea).addClass("invalid-field");
-
     } else {
-
       const $loader = $("#loader");
       const $formData = $(this).serialize();
       const $button = $(this).find("button")[0];
 
-      $("#loader").show();
-
+      $loader.show();
 
       if ($("#loader").is(":visible")) {
         $($button).hide();
+      }
 
-      } 
-
-      
-      $.post("/tweets/", $formData, function (data, status) {
+      $.post("/tweets/", $formData, function() {
         loadTweets();
       });
-      
     }
   });
 
   //Helper function respomsible for validating tweet form field
   /**
-   * 
-   * @param {*} formField The form's textarea or input field 
+   *
+   * @param {*} formField The form's textarea or input field
    * @returns An object specifying whether the form contains an error, and an error message specifying the error message to be declared
    */
-  const validateForm = function (formField) {
+  const validateForm = function(formField) {
     if (!$(formField).val()) {
       return { hasError: true, errorMsg: "Form fields cannot be empty." };
     } else if ($(formField).val().length > 140) {
@@ -108,7 +102,7 @@ $(() => {
   };
 
   //Helper function respomsible for reset of form elements
-  const resetForm = function () {
+  const resetForm = function() {
     const $textArea = $("#compose-form textarea");
     const $counter = $("#compose-form").find("#counter");
 
@@ -116,21 +110,18 @@ $(() => {
     $($counter).val("140");
     $("#tweet-text").removeClass("invalid-field");
     $(".form-error").hide();
-
   };
 
   //Function responsible for loading tweets from server
-  const loadTweets = function () {
-    $.get("/tweets/", function (data) {
+  const loadTweets = function() {
+    $.get("/tweets/", function(data) {
       renderTweets(data);
       resetForm();
       $("#loader").hide();
       $("#submit").show();
-
     });
   };
 
   //Initially load tweets
   loadTweets();
-  
 });
