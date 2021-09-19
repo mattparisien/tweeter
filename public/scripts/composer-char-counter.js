32ae2afa96065720d20bfd3db6f807326c62d81e
Matthew Parisien <matthewparisien4@gmail.com>
2021-09-19 20:22:18 +0000

    Fix syntax

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,11 +1,11 @@
-$( () => {
+$(() => {
   $("#tweet-text").on("input", function (e) {
     let dynamicCount = 140;
     let counter = $(this).parent().find("#counter")[0];
     
     const charsTyped = this.value.length;
-
     dynamicCount = dynamicCount - charsTyped;
+    
     if (dynamicCount < 0) {
       $(counter).addClass("charLimitExceeded");
     } else {
