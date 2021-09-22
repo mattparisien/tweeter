4fa3d35040a14e6dbdb85284e1af70e105aa8aed
Matthew Parisien <matthewparisien4@gmail.com>
2021-09-22 12:00:03 +0000

    Remove scroll to form helper function and move it into helpers.js module, and import it into this file.

--- public/scripts/button-animations.js
+++ public/scripts/button-animations.js
@@ -1,24 +1,7 @@
 //Button Animations File
-$(() => {
-  // Helper function: scrolls to tweet form while taking nav offset into consideration
-  /**
-   *
-   * @param {*} button Button linking to form
-   */
-  const scrollToForm = function (e) {
-    e.preventDefault();
-    $("#new-tweet").slideDown();
-      setTimeout(() => {
-        $("html, #root").animate(
-          {
-            scrollTop: $("#compose-form").offset().top - 300, //Offset top by 300px to prevent nav covering form
-          },
-          1500
-        );
-        $("#compose-form textarea").focus();  
-      }, 200);    
-  };
+import { scrollToForm } from "./helpers.js"
 
+$(() => {
   //Nav arrow link: On click event triggers a smooth scroll to tweet form
   const $button = $("#scroll-to-form");
   const $backToTweeting = $("#backtotop");
@@ -51,19 +34,15 @@ $(() => {
 
   //Reveal back to top button at bottom of page + simulatenously hide nav link
   const revealBackToTop = function () {
-
-    $(window).on("scroll", function() {
-
+    $(window).on("scroll", function () {
       if ($(window).scrollTop() >= 650) {
         $("#backtotop").fadeIn(200);
         $(".nav-right").fadeOut(200);
       } else if ($(window).scrollTop() <= 650) {
         $("#backtotop").fadeOut(200);
-        $(".nav-right").fadeIn(200)
+        $(".nav-right").fadeIn(200);
       }
-  
-    })    
-
+    });
   };
   revealBackToTop();
 });
